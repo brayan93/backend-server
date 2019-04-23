6b3eb5b70e97575755d14d57c57c1e7f93021017
Brayan <brayan.giraldo@mowin-tech.com>
2019-04-23 18:01:47 -0500

    Init backend server

---
+++ app.js
@@ -0,0 +1,25 @@
+// Requires
+var express = require('express');
+
+
+
+// Init variables
+var app = express();
+
+
+
+// Rutas
+app.get('/', ( req, res, next ) => {
+
+    res.status(200).json({
+        ok: true,
+        mensaje: 'Peticion realizada correctamente'
+    });
+
+} );
+
+
+// Escuchar peticiones
+app.listen(3030, () => {
+    console.log('Express server puerto: 3000: \x1b[32m%s\x1b[0m', 'online');
+});

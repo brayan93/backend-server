6f788d249fb641b5ded90142583398b3655e8333
Brayan <brayan.giraldo@mowin-tech.com>
2019-04-24 08:47:06 -0500

    Sección 9 terminada

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 // Requires
 var express = require('express');
+var mongoose = require('mongoose');
 
 
 
 // Init variables
 var app = express();
+
+
+// Conexion db
+mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
+
+    if (err) throw err;
+
+    console.log('Base de datos: \x1b[32m%s\x1b[0m', 'online');
+
+});
 
 
 
